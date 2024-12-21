078903fc8f24725fb0fa7c32ee99347ad568b70f
jesicaacosta <ajesicadaiana@gmail.com>
2024-12-21 17:02:20 -0300

    Primer comm

---
+++ main.py
@@ -0,0 +1,12 @@
+def multi (a , b ) -> int :
+    """ DocString Permite multiplicar dos enteros
+    Args: 
+    a(int): N1
+    b(int): N2
+    
+    Returns:
+    int: a*b
+    """
+    return a * b
+
+multi(98*300)
